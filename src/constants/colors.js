6ea88a399ca0266e29e4e273ec03c5d0daa0930d
Vjacheslav Zemlyanoy <vjacheslav.zemlianiy@fluvius.co>
2020-07-30 22:30:01 +0300

    init

---
+++ src/constants/colors.js
@@ -0,0 +1,9 @@
+export const GREEN_COLOR = '#00B768';
+
+export const BLUE_COLOR = '#0062AD';
+export const LIGHT_BLUE_COLOR = '#5D9ACA';
+export const OCEAN_COLOR = '#A0CEF1';
+
+export const BLACK_COLOR = '#F8F8FC';
+export const WHITE_COLOR = '#272727';
+export const GRAY_COLOR = '#585858';
